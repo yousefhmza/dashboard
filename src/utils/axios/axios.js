1aba35f8895ba852eb125c6c57b297ab1591c78d
yousefhmza <yousef@Yousefs-MacBook-Pro.local>
2022-08-23 18:06:42 +0200

    offers

--- src/utils/axios/axios.js
+++ src/utils/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://e-commerce-app-poc.herokuapp.com/v1/",
+  baseURL: "https://e-commerce-app-poc-2.herokuapp.com/v1/",
   headers: {
     lng: "en",
   },
